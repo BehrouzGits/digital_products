ed799a232ea7e0c028a6801f8f013ad522058afd
Behrouz <b.ayanpour@gmail.com>
2023-10-08 17:44:16 +0330

    admin changed

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+from .models import User, Province
+
+
+class MyUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields' : ('username', 'password')}),
+        (_('Personal info'), {'fields' : ('first_name', 'last_name', 'phone_number', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staf', 'is_superuser','groups', 'user_permissions')}),
+        (_('Important dates'), {'fields' : ('last_login', 'date_joined')}),
+    )
+
