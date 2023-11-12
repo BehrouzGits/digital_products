5bf7fb2f1877f7f79df03b9388eac125370f916b
Behrouz <b.ayanpour@gmail.com>
2023-11-12 18:33:43 +0330

    jwt authentication added 1402-08-21

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from .views import RegisterView, GetTokenView
 
 
@@ -7,6 +12,8 @@ from .views import RegisterView, GetTokenView
 urlpatterns = [
     path('register/',RegisterView.as_view()),
     path('get-token/', GetTokenView.as_view()),
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
 ]
