d64b94376d8028bb98d2cb14031d74736da7678f
Behrouz <b.ayanpour@gmail.com>
2023-11-14 18:31:15 +0330

    permission class added 1402-08-23

--- products/views.py
+++ products/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.permissions import IsAuthenticated , IsAuthenticatedOrReadOnly
 
 from .models import Category, Product, File
 from .serializers import CategorySerializer, ProductSerializer, FileSerializer
 
 
 class ProductListView(APIView):
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         print(request.user)
