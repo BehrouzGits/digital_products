4fe02a018d9132ea68ceb6e89e008c785c39b8f2
Behrouz <b.ayanpour@gmail.com>
2023-11-21 18:52:04 +0330

    validators, Subscriptions and admin added 08-30

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-# from 
+from utils.validators import validate_sku
 
 class Package(models.Model):
     title = models.CharField(_('title'), max_length=50)
@@ -19,4 +19,21 @@ class Package(models.Model):
         db_table = 'packages'
         verbose_name = _('Package')
         verbose_name_plural = _('Packages')
+    
+    def __str__(self):
+        return self.title
+    
+
+class Subscription(models.Model):
+    user = models.ForeignKey('users.User' , related_name = '%(class)s' , on_delete=models.CASCADE)
+    package = models.ForeignKey(Package, related_name='%(class)s', on_delete=models.CASCADE)
+    created_time = models.DateTimeField (_('created time'), auto_now_add=True)
+    expire_time = models.DateTimeField(_('expire time'), blank=True , null=True)
+
+    class Meta:
+        db_table = 'subscriptions'
+        verbose_name = _('Subscription')
+        verbose_name_plural = _('Subscriptions')
+
+
 
