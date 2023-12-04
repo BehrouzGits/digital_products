5bba0a22f70298e7b87061317219fbbbe051ca24
Behrouz <b.ayanpour@gmail.com>
2023-12-04 18:52:11 +0330

    payment view 02/09/13

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,44 @@
+import uuid
+
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
+from .models import Payment,Gateway 
+from subscriptions.models import Package
+from .serializers import GatewaySerializer
+
+
+class GatewayView(APIView):
+    def get(self, request):
+        gateways = Gateway.objects.filter(is_enable=True)
+        serializer = GatewaySerializer(gateways, many=True)
+        return Response(serializer.data)
+    
+
+class PaymentView(APIView):
+    permission_class = [IsAuthenticated]
+
+    def get(self, request):
+        gateway_id = request.query_params.get('gateway')
+        package_id = request.query_params.get('package')
+
+        try :
+            package = Package.objects.get(pk=package_id, is_enable=True)
+            gateway = Gateway.objects.get(pk=gateway_id, is_enable = True)
+        except (Package.DoesNotExist, Gateway.DoesNotExist):
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        payment = Payment.objects.create(
+            user=request.user,
+            package=package,
+            gateway=gateway,
+            price=package.price,
+            phone_number=request.user.phone_number,
+            token=str(uuid.uuid4())
+        )
+
+        
